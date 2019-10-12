cb75ea83afe3d2d7daedd77aebf9421b00b5c287
nmxi <mikki1118_2@hotmail.co.jp>
2019-10-13 01:27:36 +0900

    v1.0 - 必要箇所修正と不要なものの削除

---
+++ Assets/Scripts/VideoCreateOperator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoCreateOperator : MonoBehaviour {
+
+    private VideoCreator.VideoCreatorUnity videoCreatorUnity;
+
+    public RenderTexture texture = null;
+
+    public Text text;
+
+    private bool isRecording = false;
+
+    // Use this for initialization
+    void Start () {
+        videoCreatorUnity = new VideoCreator.VideoCreatorUnity(Application.temporaryCachePath + "/tmp.mov", true, 1080, 1920);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isRecording) return;
+
+        if (texture == null) return;
+
+        videoCreatorUnity.Append(texture);
+
+	}
+
+    public void StartRecord()
+    {
+        if (isRecording) return;
+        videoCreatorUnity.StartRecording();
+        isRecording = true;
+
+        text.text = "start recording !!";
+    }
+
+    public void FinishRecord()
+    {
+        if (!isRecording) return;
+        videoCreatorUnity.FinishRecording();
+        isRecording = false;
+
+        text.text = "finish recording !!";
+    }
+}
